0769d51d2addac77dd2056d0e69f20c2ea630850
Carolyne munyiva <munyivacarolyne571@gmail.com>
2024-03-27 13:35:09 +0300

    attempting arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+//code here!
+const divide = (a, b) => a / b;
+
+//multiplying the numer by itself
+const square =(x) => x * x
+    console.log(square(x));
+square(2);
+
+//doing addition
+
+let add = (a, b) => a + b;
